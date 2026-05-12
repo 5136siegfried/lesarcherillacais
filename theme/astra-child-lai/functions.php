02daaa59a14c46288c3cb148858dc21cfbc308af
Siegfried <siegfried.sekkai@gmail.com>
2026-05-12 21:29:12 +0200

    fix: matomo snippet

--- theme/astra-child-lai/functions.php
+++ theme/astra-child-lai/functions.php
@@ -68,18 +68,19 @@ add_action('wp_enqueue_scripts', 'lai_enqueue_scripts');
 
 function lai_matomo() {
     if (!is_admin()) { ?>
-    <script>
-      var _paq = window._paq = window._paq || [];
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u="https://5136.matomo.cloud/";
+        <script>
+        var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+     (function() {
+         var u="https://5136.matomo.cloud/";
         _paq.push(['setTrackerUrl', u+'matomo.php']);
         _paq.push(['setSiteId', '1']);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-      })();
-    </script>
+        g.async=true; g.src='https://cdn.matomo.cloud/5136.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+        </script>
     <?php }
 }
 add_action('wp_head', 'lai_matomo');
